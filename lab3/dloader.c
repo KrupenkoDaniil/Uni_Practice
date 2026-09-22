bbff45c4452cfb6513430b5bb8ffc0e7245cf96d
KrupenkoDaniil <108655963+KrupenkoDaniil@users.noreply.github.com>
2026-09-23 00:04:16 +0300

    Update dloader.c no windows

--- lab3/dloader.c
+++ lab3/dloader.c
@@ -2,30 +2,12 @@
 
 #include <stdio.h>
 
-#ifdef _WIN32
-#include <windows.h>
-#else
 #include <dlfcn.h>
-#endif
+
 
 typedef void (*lib_entry)(void);
 
 void dloader_run(const char* lib_name, const char* entry_name) {
-#ifdef _WIN32
-  HMODULE lib = LoadLibraryA(lib_name);
-  lib_entry entry;
-
-  if (lib == NULL) {
-    printf("cannot load library '%s'\n", lib_name);
-    return;
-  }
-  entry = (lib_entry)(void*)GetProcAddress(lib, entry_name);
-  if (entry == NULL)
-    printf("cannot find function '%s'\n", entry_name);
-  else
-    entry();
-  FreeLibrary(lib);
-#else
   void* lib = dlopen(lib_name, RTLD_LAZY);
   lib_entry entry;
 
@@ -39,5 +21,4 @@ void dloader_run(const char* lib_name, const char* entry_name) {
   else
     entry();
   dlclose(lib);
-#endif
 }
